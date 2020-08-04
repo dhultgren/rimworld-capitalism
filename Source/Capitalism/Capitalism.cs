5c5ba40c53f306004527d6c00f5e29b470b38e82
dhultgren <hultgren.daniel@gmail.com>
2020-08-04 23:06:44 +0200

    Add settings

--- Source/Capitalism/Capitalism.cs
+++ Source/Capitalism/Capitalism.cs
@@ -1,15 +1,39 @@
 ﻿using HarmonyLib;
 using System.Reflection;
+using UnityEngine;
 using Verse;
 
 namespace Capitalism
 {
     [StaticConstructorOnStartup]
-    public class Capitalism
+    public class Capitalism : Mod
     {
+        public static CapitalismSettings Settings;
+
         static Capitalism()
         {
             new Harmony("Capitalism").PatchAll(Assembly.GetExecutingAssembly());
         }
+
+        public Capitalism(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<CapitalismSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            var listing = new Listing_Standard();
+            listing.ColumnWidth = 400f;
+            listing.Begin(inRect);
+
+            string buffer = Settings.MaxSupplyDemandChangePercent.ToString();
+            listing.Label("Limit how much the price will fluctuate with supply and demand, in percent.");
+            listing.IntEntry(ref Settings.MaxSupplyDemandChangePercent, ref buffer);
+
+            listing.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory() => "Capitalism";
     }
 }
---
+++ Source/Capitalism/CapitalismSettings.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace Capitalism
+{
+    public class CapitalismSettings : ModSettings
+    {
+        public int MaxSupplyDemandChangePercent = 300;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref MaxSupplyDemandChangePercent, "maxSupplyDemandChangePercent");
+        }
+    }
+}
